957cc61fec6663430a48ab9181d5b603c9b0e4ce
MateuCarbonell <mcarbonell@cifpfbmoll.eu>
2024-05-13 04:42:58 +0200

    primer paso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,10 @@ public class Main {
         GestorBD gestorBD = new GestorBD();
         // Crear un vehículo para agregar a la base de datos
         Vehiculo vehiculo = new Vehiculo(1, "Toyota", "Corolla", 2022, 25000.0, "Nuevo");
+        Vehiculo vehiculo2 = new Vehiculo(2, "Mitsubishi", "EVO", 2023, 35000.0, "Usado");
 
         try{
-            gestorBD.agregarVehiculo(vehiculo);
+            gestorBD.agregarVehiculo(vehiculo2);
         }catch(Exception e){
             e.printStackTrace();
         }
